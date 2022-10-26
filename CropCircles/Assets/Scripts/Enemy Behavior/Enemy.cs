9f7e64e7fe82fed3f02163cdde42293d7cadf055
Mark Trantham <markbtrantham@yahoo.com>
2022-10-26 11:47:50 -0700

    enemy behavior update

--- CropCircles/Assets/Scripts/Enemy Behavior/Enemy.cs
+++ CropCircles/Assets/Scripts/Enemy Behavior/Enemy.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    //to be removed
+    [SerializeField] private TextMeshProUGUI iSeeYouText;
+
+    public enum EnemyState
     {
-        
+        Idle,
+        Patrol,
+        Chase,
+        Attack,
+        Rampage
     }
 
-    // Update is called once per frame
-    void Update()
+    private Rigidbody enemyRigidbody;
+    private NavMeshAgent enemyNavMeshAgent;
+    //enemy field of view script 
+    private FieldOfView enemyFOV;
+
+
+    public float enemySpeed = 9f;
+
+    [Header("NavMesh variables")]
+
+    public Transform[] waypoints;
+    private int currentWaypointIndex;
+
+    
+    
+    
+    private void Awake()
     {
-        
+        //grab components
+        enemyNavMeshAgent = GetComponent<NavMeshAgent>();
+        enemyRigidbody = GetComponent<Rigidbody>();
+        enemyFOV = GetComponent<FieldOfView>();
     }
+    
+
+    private void Update()
+    {
+        if (enemyFOV.canSeePlayer)
+        {
+            iSeeYouText.text = "I SEE YOU !!0__0!!";
+        }
+        else
+        {
+            iSeeYouText.text = "";
+        }
+    }
+
 }
