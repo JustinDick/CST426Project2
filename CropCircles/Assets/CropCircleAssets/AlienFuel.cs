3ed06d483982a73a587a1188d159a476552677e8
Ari Garcia <71861098+AriGarcia-tech@users.noreply.github.com>
2022-11-10 13:57:06 -0800

    played with barrel and crate assets & created main menu

--- CropCircles/Assets/CropCircleAssets/AlienFuel.cs
+++ CropCircles/Assets/CropCircleAssets/AlienFuel.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class AlienFuel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player;
+
+    public float pickUpRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    private void Update()
     {
-        
+        Vector3 distanceToPlayer = player.position - transform.position;
+        if (!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull) PickUp();
+
+        if (equipped && Input.GetKeyDown(KeyCode.Q)) Drop();
+    }
+    
+    private void PickUp()
+    {
+        equipped = true;
+        slotFull = true;
+
+        rb.isKinematic = true;
+        coll.isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Drop()
     {
-        
+        equipped = false;
+        slotFull = false;
+
+        rb.isKinematic = false;
+        coll.isTrigger = false;
     }
 }
