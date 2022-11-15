e688abc3a35d26b8c6cb4201490fd8e5877f017a
Ari Garcia <71861098+AriGarcia-tech@users.noreply.github.com>
2022-11-15 14:53:25 -0800

    Tweaked AlienFuel to include slider & made minor changes to the map
    Drew a border with a different grass detail and re-added two plant variants into the empty patch that I had.

--- CropCircles/Assets/CropCircleAssets/AlienFuel.cs
+++ CropCircles/Assets/CropCircleAssets/AlienFuel.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AlienFuel : MonoBehaviour
 {
+    public Slider slider;
+
+    public int maxFuel = 100;
+    public int currentFuel;
+
+    public AlienFuel fuelBar;
+
     public Rigidbody rb;
     public BoxCollider coll;
     public Transform player;
@@ -14,12 +22,36 @@ public class AlienFuel : MonoBehaviour
     public bool equipped;
     public static bool slotFull;
 
+    private void Start()
+    {
+        currentFuel = 0;
+        //fuelBar.SetMaxFuel(maxFuel);
+    }
+
+    public void SetFuel(int fuel)
+    {
+        slider.value = fuel;
+    }
+
+    public void SetMaxFuel(int fuel)
+    {
+        //slider.maxValue = fuel;
+        slider.value = fuel;
+    }
+
     private void Update()
     {
         Vector3 distanceToPlayer = player.position - transform.position;
         if (!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull) PickUp();
 
         if (equipped && Input.GetKeyDown(KeyCode.Q)) Drop();
+    }
+
+    void FuelUp(int eating)
+    {
+        currentFuel += eating;
+
+        fuelBar.SetFuel(currentFuel);
     }
     
     private void PickUp()
